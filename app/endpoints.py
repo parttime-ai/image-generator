4737239dd79d7875156c1d06e86ba84b4cb89b9d
Hien Nguyen <hien-nguyen@mail.de>
2024-11-11 12:45:07 +0100

    feat: remove obsolete code

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,7 +1,6 @@
 import base64
 import logging
 from io import BytesIO
-from typing import Any
 
 import PIL.Image
 from PIL.Image import Image
@@ -9,7 +8,6 @@ from fastapi import APIRouter, HTTPException, Request
 
 from app.api import IImageNsfwClassifier, NsfwPrediction, Nsfw, ITextNsfwClassifier
 from app.config import AppConfiguration
-
 from app.models.requests import ImageRequest, TextPrompt
 from app.models.response import ContentAssessment, OverallAssessment, ConfidenceLevel
 from app.nsfw_detection.text_classifier import MixtureOfAgentsClassifier
@@ -73,40 +71,6 @@ async def generate_image_together(request: Request, image_request: ImageRequest)
     except Exception as e:
         logging.error(f"Error: {e}")
         raise HTTPException(status_code=500, detail="Could not classify text")
-
-
-@router.post("/generate-image/local-sd")
-async def generate_image_local_sd(request: Request, image_request: ImageRequest) -> str:
-    model = request.app.state.stable_diffusion
-    image = await model.generate(
-        prompt=image_request.prompt,
-        width=image_request.width,
-        height=image_request.height,
-        num_inference_steps=image_request.num_inference_steps,
-        guidance_scale=image_request.guidance_scale,
-        seed=image_request.seed
-    )
-
-    if image is None:
-        raise HTTPException(status_code=500, detail="Could not generate image")
-
-    return image
-
-
-@router.post("/nsfw-text-detection/distil-clf")
-async def nsfw_text_detection_distil(request: Request, text_prompt: TextPrompt) -> Any:
-    model = request.app.state.distil_clf
-    prediction = await model.classify(text_prompt.prompt)
-
-    return prediction
-
-
-@router.post("/nsfw-text-detection/roberta-clf")
-async def nsfw_text_detection_roberta(request: Request, text_prompt: TextPrompt) -> Any:
-    model = request.app.state.roberta_clf
-    prediction = await model.classify(text_prompt.prompt)
-
-    return prediction
 
 
 @router.post("/nsfw-text-detection/moa-clf")
